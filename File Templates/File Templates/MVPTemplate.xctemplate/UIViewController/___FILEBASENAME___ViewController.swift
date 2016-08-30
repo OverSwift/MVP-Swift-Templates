08af3fd0f01bbddd66641cbb93956afac8832915
Sergiy Loza <kabal2015@gmail.com>
2016-08-30 19:29:54 +0300

    Add basic MVP template with view controller and presenter

---
+++ File Templates/File Templates/MVPTemplate.xctemplate/UIViewController/___FILEBASENAME___ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+import UIKit
+
+protocol ___FILEBASENAMEASIDENTIFIER___ViewProtocol: class {
+  
+}
+
+class ___FILEBASENAMEASIDENTIFIER___ViewController: UIViewController, ___FILEBASENAMEASIDENTIFIER___ViewProtocol {
+  
+  // MARK: Public properties
+  
+  lazy var presenter:___FILEBASENAMEASIDENTIFIER___Presenter = ___FILEBASENAMEASIDENTIFIER___Presenter(view: self)
+  // MARK: Private properties
+  
+  // MARK: View lifecycle
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+  }
+  
+  // MARK: Display logic
+  
+  // MARK: Actions
+  
+  // MARK: Private functions
+}
